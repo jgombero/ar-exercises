564389249123f9d9d8a948071db6812e5efc7865
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-21 04:13:10 +0000

    Load subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,16 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surrey = Store.create(name: 'Surrey', annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+
+whistler = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: false)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
+for store in @mens_stores do
+  puts store.name, store.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: 1000000..-Float::INFINITY)
